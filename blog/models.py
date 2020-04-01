d68ccdc91af4780cdbe58a3b74b5bb04707d08c8
mykucukkara <mykucukkara@gmail.com>
2020-04-01 22:29:08 +0300

    Added Blog

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    keywords = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='childiren', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
