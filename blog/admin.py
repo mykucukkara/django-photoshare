6fe58cac89cc261b6e9a5831b1f55f6d8c0c241a
mykucukkara <mykucukkara@gmail.com>
2020-04-01 22:43:25 +0300

    Added Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from blog.models import Category
+#admin panelde arayüz işlemleri
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+admin.site.register(Category,CategoryAdmin)
