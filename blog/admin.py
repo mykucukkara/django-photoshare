8df487a79900271893ff8cb7b9552e5206f09847
mykucukkara <mykucukkara@gmail.com>
2020-04-02 21:41:39 +0300

    Added Blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Category
+from blog.models import Category, Blog
+
+
 #admin panelde arayüz işlemleri
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 admin.site.register(Category,CategoryAdmin)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'status']
+    list_filter = ['status']
+admin.site.register(Blog,BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Blog(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with category table (UP)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    uptade_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
