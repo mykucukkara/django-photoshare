15dac491824706bdd6ef7a3580e7f7b77acf8eff
mykucukkara <mykucukkara@gmail.com>
2020-05-01 07:28:24 +0300

    Added Contact Form(+some bugs)

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
+from django.forms import ModelForm, TextInput, Textarea
+
 
 # Create your models here.
 class Setting(models.Model):
@@ -25,6 +27,7 @@ class Setting(models.Model):
     twitter = models.CharField(blank=True, max_length=50)
     aboutus = RichTextUploadingField(blank=True)
     contact = RichTextUploadingField(blank=True)
+    contact_map = RichTextUploadingField(blank=True)
     references = RichTextUploadingField(blank=True)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
@@ -32,3 +35,35 @@ class Setting(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+    )
+    name = models.CharField(blank=True, max_length=50)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=250)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=50)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    uptade_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': TextInput(attrs={'class': 'span4','size': 16, 'type':'text', 'placeholder': 'Name & Surname'}),
+            'subject': TextInput(attrs={'class': 'span4','size': 16, 'type':'text', 'placeholder': 'Subject'}),
+            'email': TextInput(attrs={'class': 'span4','size': 16, 'type':'text', 'placeholder': 'Email'}),
+            'message': Textarea(attrs={'class': 'span4','size': 16, 'type':'text', 'placeholder': 'Message'})
+
+        }
